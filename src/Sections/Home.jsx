2e16e58adedaec0ebea64c9b5de90f43708df00a
Rishabhsoni15 <sonirishu42@gmail.com>
2025-06-20 15:04:15 +0530

    tech330

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -1,6 +1,50 @@
+// import React from "react";
+// import EnterAnimation from "../component/EnterAnimation.jsx";
+// ////enter sonethejewlf
+// import Banner from "../Component/Banner.jsx";
+// import Cowork from "../Component/Cowork.jsx";
+// import Ourcard from "../Component/Ourcard.jsx";
+// import Workspace from "../Component/Workspace.jsx";
+// import Faq from "../Component/Faq.jsx";
+// import Cards from "../Component/Cards.jsx";
+// import Experience from "../Component/Experience.jsx";
+// import Positivity from "../Component/Positivity.jsx";
+// import Saying from "../Component/Saying.jsx";
+// import ReviewCard from "../Component/ReviewCard.jsx";
+// import Blogs from "../Component/Blogs.jsx";
+// import Tiup from "../Component/Tiup.jsx";
+// import Footer1 from "../Component/Footer1.jsx";
+// import Downpage from "../Component/Downpage.jsx";
+// import WhatsappButton from "../Component/WhatsappButton.jsx";
+// import NavBar from "../component/NavBar.jsx";
+// function App() {
+//   return (
+//     <>
+//       <EnterAnimation />
+//       <NavBar />
+//       <WhatsappButton />
+//       <Banner />
+//       <Cowork />
+//       <Ourcard />
+//       <Workspace />
+//       <Faq />
+//       <Cards />
+//       <Experience />
+//       <Positivity />
+//       <Saying />
+//       <Downpage />
+//       <ReviewCard />
+//       <Blogs />
+//       <Tiup />
+//       <Footer1 />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import EnterAnimation from "../component/EnterAnimation.jsx";
-////enter sonethejewlf
+import EnterAnimation from "../Component/EnterAnimation.jsx";
 import Banner from "../Component/Banner.jsx";
 import Cowork from "../Component/Cowork.jsx";
 import Ourcard from "../Component/Ourcard.jsx";
@@ -16,7 +60,8 @@ import Tiup from "../Component/Tiup.jsx";
 import Footer1 from "../Component/Footer1.jsx";
 import Downpage from "../Component/Downpage.jsx";
 import WhatsappButton from "../Component/WhatsappButton.jsx";
-import NavBar from "../component/NavBar.jsx";
+import NavBar from "../Component/NavBar.jsx";
+
 function App() {
   return (
     <>
